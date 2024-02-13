3baee9b078e4e2a8e8ad338adadcc87958375d29
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-02-13 09:33:27 -0500

    server is up and running

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+const connection = require('./config/connection');
+
+
+// Middleware
+app.use(express.json());
+
+connection.on('open', () => {
+  app.listen(PORT, () => console.log('server started on port', PORT));
+});
