d577e6dab9599fcfc2ba3485198db2fcd3bcbbc6
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-02-13 16:00:13 -0500

    added all files needed for the app

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect('mongodb://127.0.0.1:27017/social_network_db');
 
 module.exports = mongoose.connection;
