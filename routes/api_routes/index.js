6130c80560adf46a0560ca037cd628734bdcd1d2
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-02-13 19:36:48 -0500

    loaded routes to server

---
+++ routes/api_routes/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  user_routes: require('./user_routes'),
+  thought_routes: require('./thought_routes')
+}
--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const PORT = process.env.PORT || 4444;
 
 const connection = require('./config/connection');
 
+const { user_routes, thought_routes } = require('./routes/api_routes')
 
-// Middleware
+
+// MIDDLEWARE
 app.use(express.json());
+
+// LOAD ROUTES
+app.use('/api', [
+  user_routes,
+  thought_routes
+])
 
 connection.on('open', () => {
   app.listen(PORT, () => console.log('server started on port', PORT));
